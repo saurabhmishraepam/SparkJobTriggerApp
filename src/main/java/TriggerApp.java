9c56ed351a15764b29d939b771ca551e7ceffe51
Saurabh Mishra <Saurabh_Mishra@epam.com>
2019-11-08 18:51:09 +0530

    app

--- src/main/java/TriggerApp.java
+++ src/main/java/TriggerApp.java
@@ -4,35 +4,37 @@ import java.io.InputStreamReader;
 
 public class TriggerApp {
 
-    public static void main(String [] args){
+    public static void main(String [] args) throws IOException, InterruptedException {
+        StringBuffer output = new StringBuffer();
+
+        Process p =Runtime.getRuntime().exec("C:\\Users\\Saurabh_Mishra\\Documents\\gigaspaces\\gigaspaces-insightedge-enterprise-14.5.0\\gigaspaces-insightedge-enterprise-14.5.0\\insightedge\\bin\\insightedge-submit.cmd" +
+                " --class com.gigaspaces.SpacePersistApp" +
+                " --master spark://127.0.0.1:7077" +
+                " C:\\Users\\Saurabh_Mishra\\OneDrive - EPAM\\gigaspace\\spark-submit\\insightedgeexamples2\\insightedgeexamples2\\target\\SpaceConnectorsApp-1.0-SNAPSHOT-jar-with-dependencies.jar");
+
+        p.waitFor();
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+        String line = "";
+        while ((line = reader.readLine())!= null) {
+            output.append(line + "\n");
+        }
+        System.out.println("till "+output);
+
 
 
         ProcessBuilder processBuilder = new ProcessBuilder();
         // Windows
-        processBuilder.command("cmd.exe", "C:\\Users\\Saurabh_Mishra\\Documents\\gigaspaces\\gigaspaces-insightedge-enterprise-14.5.0\\gigaspaces-insightedge-enterprise-14.5.0\\insightedge\\bin\\insightedge-submit.cmd", "");
+       /* processBuilder.command("C:\\Users\\Saurabh_Mishra\\Documents\\gigaspaces\\gigaspaces-insightedge-enterprise-14.5.0\\gigaspaces-insightedge-enterprise-14.5.0\\insightedge\\bin\\insightedge-submit.cmd" +
+                        " --class com.gigaspaces.SpacePersistApp" +
+                        " --master spark://127.0.0.1:7077" +
+                        " C:\\Users\\Saurabh_Mishra\\OneDrive - EPAM\\gigaspace\\spark-submit\\insightedgeexamples2\\insightedgeexamples2\\target\\SpaceConnectorsApp-1.0-SNAPSHOT-jar-with-dependencies.jar", "c/", ""
+                );*/
 
 
 
-        try {
 
-            Process process = processBuilder.start();
-
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-            int exitCode = process.waitFor();
-            System.out.println("\nExited with error code : " + exitCode);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
     }
 
